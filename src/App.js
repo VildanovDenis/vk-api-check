d4bda5c451fc9c24c30b3e55e55c91fd6973ad46
Denis <denis@Air-Denis.Dlink>
2020-01-29 21:58:55 +0300

    Removes useless files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import React from 'react';
-import {useSpring} from 'react-spring'
-
-import { SomeArrayItem } from './containers/someArrayItem';
 
 import './App.css';
 
-const someArr = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-    31, 32, 33, 34, 35, 36, 37, 38, 39, 40
-];
-
 function App() {
-    const props = useSpring({opacity: 1, from: {opacity: 0}});
-
     const onClick = () => {
         if(window.VK) {
             window.VK.Auth.login(res => console.log(res))
@@ -23,9 +11,6 @@ function App() {
     return (
         <div className='App'>
             <button type='button' onClick={onClick}>123</button>
-            {
-                someArr.map(item => <SomeArrayItem item={item} key={item}/>)
-            }
         </div>
     );
 }
