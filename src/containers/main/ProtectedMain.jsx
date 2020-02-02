7bc677aa3db3d44d3c37ef3a8b03d08fa4aeb058
Denis <denis@Air-Denis.Dlink>
2020-02-02 22:25:36 +0300

    Adds self info

--- src/containers/main/ProtectedMain.jsx
+++ src/containers/main/ProtectedMain.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import { SearchBar } from '../../components/searchBar';
 import { FriendCard } from '../../components/friendCard';
@@ -8,16 +8,31 @@ import { VkService } from '../../services/Vk';
 
 export const ProtectedMain = () => {
     const [state, setState] = useState([]);
+    const [loginedUser, setLoginedUser] = useState(null);
     const [searchString, setSearchString] = useState('');
     const routerHistory = useHistory();
     
     useEffect(() => {
-        const method = 'friends.get';
-        const params = {fields: 'first_name, last_name, photo_100'};
+        const loginedUserId = VkService.getSession().mid;
+        const method = 'users.get';
+        const params = {
+            user_ids: loginedUserId,
+            fields: 'photo_200'
+        };
         VkService.callApi(method, params)
-            .then(res => setState(res.response.items))
-            .catch(err => console.error(err))
+            .then(res => setLoginedUser(res.response[0]))
+            .catch(err => console.error(err));        
     }, []);
+
+    useEffect(() => {
+        if (loginedUser !== null) {
+            const method = 'friends.get';
+            const params = {fields: 'first_name, last_name, photo_100'};
+            VkService.callApi(method, params)
+                .then(res => setState(res.response.items))
+                .catch(err => console.error(err))
+        }
+    }, [loginedUser]);
 
     const onLogoutClick = () => (
         VkService.killAuth()
@@ -68,10 +83,28 @@ export const ProtectedMain = () => {
 
     return (
         <div className='main'>
-            <Link to='/login'>123</Link>
             <h1 className='main__title'>My friends</h1>
             <button type='button' onClick={onLogoutClick}>Выйти</button>
             <SearchBar setSearchString={setSearchString}/>
+            {
+                loginedUser &&
+                <div>
+                    <a
+                        href={`https://vk.com/id${loginedUser.id}`}
+                        target='_blank'
+                        rel='noreferrer noopener'
+                        className='main__card'>
+                        <img
+                            src={loginedUser.photo_200}
+                            alt={`Аватар пользователя ${loginedUser.first_name}`}
+                            width='200'
+                            height='200'/>
+                        <p>
+                            {`${loginedUser.first_name} ${loginedUser.last_name}`}
+                        </p>
+                    </a>
+                </div>
+            }
             {
                 searchString &&
                 <p>Результаты поиска по запросу: "{searchString}"</p>
