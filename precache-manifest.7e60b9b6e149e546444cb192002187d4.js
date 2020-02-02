f0cdbd9ac5910f24513cc08aa73e42b63da60a59
Denis <denis@Air-Denis.Dlink>
2020-02-03 00:12:35 +0300

    Updates

---
+++ precache-manifest.7e60b9b6e149e546444cb192002187d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aa0cc5e03a07d6cfd0657ebda434b49",
+    "url": "/vk-api-check/index.html"
+  },
+  {
+    "revision": "0bbb005a3deb3ed31823",
+    "url": "/vk-api-check/static/css/main.4376c0f2.chunk.css"
+  },
+  {
+    "revision": "3b40531dbb4c753142a2",
+    "url": "/vk-api-check/static/js/2.23cda42d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/vk-api-check/static/js/2.23cda42d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0bbb005a3deb3ed31823",
+    "url": "/vk-api-check/static/js/main.ab513f4d.chunk.js"
+  },
+  {
+    "revision": "ec3414526f4a118b49b2",
+    "url": "/vk-api-check/static/js/runtime-main.1ee311e1.js"
+  }
+]);
