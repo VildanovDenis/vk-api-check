c5660a4b07ebdf8c03e41a336d979f793e18c9fc
Denis <denis@Air-Denis.Dlink>
2020-02-03 21:29:10 +0300

    Adds basename

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { VkService } from './services/Vk';
 VkService.subscribeVkInit(7299169);
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename='/vk-api-check'>
         <App />
     </BrowserRouter>
     , document.getElementById('root')
